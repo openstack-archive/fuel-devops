46377880f139f2f22f50d5f81b9a8dbe72cc71f7
vic <vgalkin@mirantis.com>
2013-01-04 10:00:51 +0400

    rdoc for driver

--- src/devops/driver/libvirt/libvirt_driver.py
+++ src/devops/driver/libvirt/libvirt_driver.py
@@ -6,11 +6,8 @@ from src.devops.helpers import scancodes
 from src.devops.helpers.retry import retry
 import xml.etree.ElementTree as ET
 import ipaddr
-from src.devops.models import Node
+from src.devops.models import Node, Volume
 
-
-class LibvirtException(Exception):
-    pass
 
 class LibvirtDriver:
     def __init__(self, name, xml_builder=LibvirtXMLBuilder()):
@@ -19,38 +16,58 @@ class LibvirtDriver:
         self.conn = libvirt.open(name)
         self.capabilities = None
 
+    @retry()
     def get_capabilities(self):
+        """
+        :rtype : ET
+        """
         if self.capabilities is None:
             self.capabilities = self.conn.getCapabilities()
         return ET.fromstring(self.capabilities)
 
     @retry()
-    def bridge_name(self, network):
+    def network_bridge_name(self, network):
+        """
+        :rtype : None
+        """
         self.conn.networkLookupByUUIDString(network.uuid).bridgeName()
 
     @retry()
-    def create_network(self, network):
+    def network_create(self, network):
+        """
+        :rtype : None
+        """
         network.uuid = self.conn.networkDefineXML(
             self.xml_builder.build_network_xml(network)
-        ).UUID
+        ).UUID()
 
     @retry()
-    def delete_network(self, network):
+    def network_delete(self, network):
+        """
+        :rtype : None
+        """
         self.conn.networkLookupByUUID(network.uuid).destroy()
         self.conn.networkLookupByUUID(network.uuid).undefine()
 
     @retry()
-    def start_network(self, network):
+    def network_start(self, network):
+        """
+        :rtype : None
+        """
         self.conn.networkLookupByUUIDString(network.uuid).create()
 
     @retry()
-    def stop_network(self, network):
-        self.conn.networkLookupByUUIDString(network.uuid).destroy()
-
-    def create_node(self, node):
+    def network_stop(self, network):
         """
         :rtype : None
+        """
+        self.conn.networkLookupByUUIDString(network.uuid).destroy()
+
+    @retry()
+    def node_create(self, node):
+        """
         :type node: Node
+        :rtype : None
         """
         emulator = self.get_capabilities(
         ).find(
@@ -59,78 +76,165 @@ class LibvirtDriver:
         node_xml = self.xml_builder.build_node_xml(node, emulator)
         self.uuid = self.conn.createXML(node_xml, 0).UUID()
 
-    def delete_node(self, node):
+    @retry()
+    def node_delete(self, node):
+        """
+        :type node: Node
+        :rtype : None
+        """
         self.conn.lookupByUUID(node.uuid).destroy()
         self.conn.lookupByUUID(node.uuid).undefine()
 
-    def get_vnc_port(self, node):
+    @retry()
+    def node_get_vnc_port(self, node):
+        """
+        :type node: Node
+        :rtype : String
+        """
         xml_desc = ET.fromstring(self.conn.lookupByUUID(node.uuid).XMLDesc(0))
         vnc_element = xml_desc.find('devices/graphics[@type="vnc"][@port]')
         if vnc_element:
             return vnc_element.get('port')
 
-    def start_node(self, node):
+    @retry()
+    def node_start(self, node):
+        """
+        :type node: Node
+        :rtype : None
+        """
         self.conn.lookupByUUID(node.uuid).create()
 
-    def stop_node(self, node):
-        self.destroy_node(node)
-
-    def reset_node(self, node):
+    @retry()
+    def node_reset(self, node):
+        """
+        :type node: Node
+        :rtype : None
+        """
         self.conn.lookupByUUID(node.uuid).reset()
 
-    def reboot_node(self, node):
+    @retry()
+    def node_reboot(self, node):
+        """
+        :type node: Node
+        :rtype : None
+        """
         self.conn.lookupByUUID(node.uuid).reboot()
 
-    def suspend_node(self, node):
+    @retry()
+    def node_suspend(self, node):
+        """
+        :type node: Node
+        :rtype : None
+        """
         self.conn.lookupByUUID(node.uuid).suspend()
 
-    def resume_node(self, node):
+    @retry()
+    def node_resume(self, node):
+        """
+        :type node: Node
+        :rtype : None
+        """
         self.conn.lookupByUUID(node.uuid).resume()
 
-    def shutdown_node(self, node):
+    @retry()
+    def node_shutdown(self, node):
+        """
+        :type node: Node
+        :rtype : None
+        """
         self.conn.lookupByUUID(node.uuid).shutdown()
 
-    def destroy_node(self, node):
+    @retry()
+    def node_destroy(self, node):
+        """
+        :type node: Node
+        :rtype : None
+        """
         self.conn.lookupByUUID(node.uuid).destroy()
 
-    def get_node_snapshots(self, node):
-        self.conn.lookupByUUID(node.uuid).snapshotListNames(0)
+#    @retry()
+    def node_get_snapshots(self, node):
+        """
+        :rtype : List
+        :type node: Node
+        """
+        return self.conn.lookupByUUID(node.uuid).snapshotListNames(0)
 
-    def create_snapshot(self, node, name=None, description=None):
-        xml =self.xml_builder.build_snapshot_xml(name, description)
+    @retry()
+    def node_create_snapshot(self, node, name=None, description=None):
+        """
+        :type description: String
+        :type name: String
+        :type node: Node
+        :rtype : None
+        """
+        xml = self.xml_builder.build_snapshot_xml(name, description)
         self.conn.lookupByUUID(node.uuid).snapshotCreateXML(xml)
 
+    @retry()
     def _get_snapshot(self, domain, name):
+        """
+        :type name: String
+        :rtype : libvirt.virDomainSnapshot
+        """
         if name is None:
             return domain.snapshotCurrent()
         else:
             return domain.snapshotLookupByName(name, 0)
 
-    def revert_snapshot(self, node, name=None):
+    @retry()
+    def node_revert_snapshot(self, node, name=None):
+        """
+        :type node: Node
+        :type name: String
+        :rtype : None
+        """
         domain = self.conn.lookupByUUID(node.uuid)
         snapshot = self._get_snapshot(domain, name)
-        domain.revertToSnapshot(snapshot,0)
+        domain.revertToSnapshot(snapshot, 0)
 
-    def delete_snapshot(self, node, name=None):
+    @retry()
+    def node_delete_snapshot(self, node, name=None):
+        """
+        :type node: Node
+        :type name: String
+        """
         domain = self.conn.lookupByUUID(node.uuid)
         snapshot = self._get_snapshot(domain, name)
         snapshot.delete(0)
 
-    def send_keys_to_node(self, node, keys):
+    @retry()
+    def node_send_keys(self, node, keys):
+        """
+        :rtype : None
+        :type node: Node
+        """
+
         keys = scancodes.from_string(str(keys))
         for key_codes in keys:
             if isinstance(key_codes[0], str):
                 if key_codes[0] == 'wait':
                     sleep(1)
                 continue
-            self.conn.lookupByUUID(node.uuid).sendKey(0, 0, key_codes, len(key_codes), 0, 0)
+            self.conn.lookupByUUID(node.uuid).sendKey(0, 0, key_codes,
+                len(key_codes), 0, 0)
 
-    def create_volume(self, volume, libvirt_pool='default'):
-        libvirt_pool = self.conn.storagePoolLookupByName(libvirt_pool)
-        libvirt_volume = libvirt_pool.createXML(self.xml_builder.build_volume_xml(volume))
+    @retry()
+    def volume_create(self, volume, pool='default'):
+        """
+        :type volume: Volume
+        :type pool: String
+        :rtype : None
+        """
+        libvirt_volume = self.conn.storagePoolLookupByName(pool).createXML(
+            self.xml_builder.build_volume_xml(volume))
         volume.uuid = libvirt_volume.key()
 
     def _get_file_size(self, file):
+        """
+        :type file: file
+        :rtype : int
+        """
         current = file.tell()
         try:
             file.seek(0, 2)
@@ -139,19 +243,30 @@ class LibvirtDriver:
             file.seek(current)
         return size
 
-    def upload_volume(self, volume, path):
+    @retry(count=2)
+    def volume_upload(self, volume, path):
         with open(path, 'rb') as f:
             self.conn.storageVolLookupByKey(volume.uuid).upload(
-                stream = f, offset = 0,
-                length = self._get_file_size(f), flags = 0)
+                stream=f, offset=0,
+                length=self._get_file_size(f), flags=0)
 
-    def delete_disk(self, disk):
-        self.conn.storageVolLookupByKey(disk.uuid)
+    @retry()
+    def volume_delete(self, volume):
+        """
+        :type volume: Volume
+        :rtype : None
+        """
+        self.conn.storageVolLookupByKey(volume.uuid)
 
+    @retry()
     def get_allocated_networks(self):
-        allocated_networks =[]
+        """
+        :rtype : List
+        """
+        allocated_networks = []
         for network_name in self.conn.listDefinedNetworks():
-            et = ET.fromstring(self.conn.networkLookupByName(network_name).XMLDesc())
+            et = ET.fromstring(
+                self.conn.networkLookupByName(network_name).XMLDesc())
             ip = et.find('ip[@address]')
             if ip:
                 address = ip.get('address')
@@ -159,5 +274,3 @@ class LibvirtDriver:
                 allocated_networks.append(ipaddr.IPNetwork(
                     "{0:>s}/{1:>s}".format(address, prefix_or_netmask)))
         return allocated_networks
-
-
