0c2941015f65658da2df0e1aabd34429068fdeb4
vic <vgalkin@mirantis.com>
2013-01-21 03:04:04 +0400

    The next intermediate state

--- src/devops/shell.py
+++ src/devops/shell.py
@@ -1,12 +1,3 @@
-#class ControllerSingleton(Controller):
-#    _instance = None
-#
-#    def __new__(cls, *args, **kwargs):
-#        if not cls._instance:
-#            cls._instance = super(ControllerSingleton, cls).__new__(
-#                cls, *args, **kwargs)
-#        return cls._instance
-
 import argparse
 import os
 from devops.manager import Manager
