1b63007bd47101ccb2b1f22bb73cc870182fffe4
nfedotov <nfedotov@mirantis.com>
2014-03-25 16:32:15 +0400

    Configurable logger
    Change-Id: I20d114017e6f05f2bc98a9375f61b4eac9ce0f93

--- devops/__init__.py
+++ devops/__init__.py
@@ -11,3 +11,32 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
+import os
+import yaml
+import logging
+import logging.config
+
+
+log_path = (os.environ.get('DEVOPS_LOG_CONFIG', os.curdir),
+            os.path.dirname(os.path.abspath(__file__)),
+            os.path.join(os.path.expanduser("~"), '.devops'),
+            '/etc/devops')
+for loc in log_path:
+    try:
+        with open(os.path.join(loc, 'log.yaml')) as f:
+            config = yaml.load(f.read())
+            # Create log folders
+            for h in config['handlers'].values():
+                if h.get('filename'):
+                    h['filename'] = os.path.expanduser(h['filename'])
+                    directory = os.path.dirname(h['filename'])
+                    if not os.path.exists(directory):
+                        os.makedirs(directory)
+
+            logging.config.dictConfig(config)
+            break
+    except IOError:
+        pass
+
+logger = logging.getLogger(__name__)
