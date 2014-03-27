ddb3c1e0bc3bcfc50daa1dfb598d866604f078b3
Igor Shishkin <ishishkin@mirantis.com>
2014-03-27 22:51:48 +0400

    devops minor version increment
    Change-Id: I6d943c2df848a5322280311fcce52dde24c0c9d3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import find_packages
 
 setup(
     name='devops',
-    version='2.2',
+    version='2.3',
     description='Library for creating and manipulating virtual environments',
     author='Mirantis, Inc.',
     author_email='product@mirantis.com',
