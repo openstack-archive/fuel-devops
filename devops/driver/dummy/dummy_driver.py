8e3c8bd246204545e8722482b8db78e85a4ed41c
Andrew Woodward <awoodward@mirantis.com>
2014-05-07 00:12:40 -0700

    Update copyright year properly
    In https://review.openstack.org/#/c/68883/ copyright year was changed, but
     not in correct format. This updates the copyright to a year range on the
     files that where modified in that commit.
    
    Change-Id: Ie0f76191cfed742eeeee0ddc4466022ad4cf637f

--- devops/driver/dummy/dummy_driver.py
+++ devops/driver/dummy/dummy_driver.py
@@ -1,4 +1,4 @@
-#    Copyright 2014 Mirantis, Inc.
+#    Copyright 2013 - 2014 Mirantis, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -11,7 +11,6 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-
 
 class DevopsDriver(object):
 
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,7 +57,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'devops'
-copyright = u'2014, Mirantis Inc.'
+copyright = u'2013 - 2014, Mirantis Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
