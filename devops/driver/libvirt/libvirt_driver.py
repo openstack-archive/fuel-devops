openstack-archive/fuel-devops

commit 37794d367d36ed147c3c9ea89bc594ccb345fe61

Workaround for virtualbox domain resuming
Change-Id: I0675f0ab7a82d4928aa44427250ddb1f0c7d85c2
Closes-Bug: #1376791

devops/driver/libvirt/libvirt_driver.py
@@ -327,7 +327,9 @@ class DevopsDriver(object):
         :type node: Node
             :rtype : None
         """
-        self.conn.lookupByUUIDString(node.uuid).resume()
+        domain = self.conn.lookupByUUIDString(node.uuid)
+        if domain.state == libvirt.VIR_DOMAIN_PAUSED:
+            domain.resume()
 
     @retry()
     def node_shutdown(self, node):

commit fb52f65023129ba700f8a57d8b6de992e6301ac1

Fix for wrong domain state comparison
This is a fix for error introduced by
commit 37794d367d36ed147c3c9ea89bc594ccb345fe61
Paused domain was never resumed due to a wrong
domain state comparison.

Change-Id: Ibb31811ee1eed7c8b83b53ded97da057b3472276

devops/driver/libvirt/libvirt_driver.py
@@ -328,7 +328,7 @@ class DevopsDriver(object):
             :rtype : None
         """
         domain = self.conn.lookupByUUIDString(node.uuid)
-        if domain.state == libvirt.VIR_DOMAIN_PAUSED:
+        if domain.state()[0] == libvirt.VIR_DOMAIN_PAUSED:
             domain.resume()
 
     @retry()