de6b66fc14ff05a89d1f274495951c2ba8280af1
Pawel Brzozowski <pbrzozowski@mirantis.com>
2015-02-06 19:49:39 +0100

    Added created field to all of the objects
    - added --timestamps switch to list command
    
    Partial-Bug: #1415024
    Change-Id: I98529b08dd42a34be94046e9a2706fcf6a18c2da

--- devops/shell.py
+++ devops/shell.py
@@ -30,7 +30,7 @@ class Shell(object):
         self.commands.get(self.params.command)(self)
 
     def do_list(self):
-        env_list = Environment.list().values('name')
+        env_list = Environment.list().values('name', 'created')
         for env in env_list:
             if self.params.list_ips:
                 cur_env = Environment.get(name=env['name'])
@@ -39,6 +39,9 @@ class Shell(object):
                     admin_ip = (cur_env.get_node(name='admin').
                                 get_ip_address_by_network_name('admin'))
                 print('{0}\t{1}'.format(env['name'], admin_ip))
+            elif self.params.timestamps:
+                created_text = env['created'].strftime('%Y-%m-%d_%H:%M:%S')
+                print('{0} {1}'.format(env['name'], created_text))
             else:
                 print(env['name'])
 
@@ -170,6 +173,11 @@ class Shell(object):
                                      action='store_const', const=True,
                                      help='show admin node ip addresses',
                                      default=False)
+        timestamps_parser = argparse.ArgumentParser(add_help=False)
+        timestamps_parser.add_argument('--timestamps', dest='timestamps',
+                                       action='store_const', const=True,
+                                       help='show creation timestamps',
+                                       default=False)
         parser = argparse.ArgumentParser(
             description="Manage virtual environments. "
                         "For addional help use command with -h/--help")
@@ -177,7 +185,7 @@ class Shell(object):
                                            help='available commands',
                                            dest='command')
         subparsers.add_parser('list',
-                              parents=[list_ips_parser],
+                              parents=[list_ips_parser, timestamps_parser],
                               help="Show virtual environments",
                               description="Show virtual environments on host")
         subparsers.add_parser('show', parents=[name_parser],
