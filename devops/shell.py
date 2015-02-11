5c3924507b207ad316b1a4be30d98ff27e2112a5
Dmitry Tyzhnenko <dtyzhnenko@mirantis.com>
2015-02-11 10:21:40 +0200

    Impove dos.py for manual manipulation with environment
    Added new actions to dos.py:
       create               Create new environment
       slave-add            Add node to env
       slave-change         Change node vcpu and memory
       slave-remove         Remove node from environment
       admin-setup          Setup admin node
    
    Change-Id: I6a5ff908f316c3fabca53200ccf29476565d892a
    Blueprint: fuel-devops-manual-env-manipulation

--- devops/shell.py
+++ devops/shell.py
@@ -14,16 +14,24 @@
 
 import argparse
 import os
+import sys
+
+import ipaddr
 
 import devops
+from devops.helpers.helpers import _get_file_size
 from devops.helpers.helpers import sync_node_time
+from devops.helpers import node_manager
 from devops.models import Environment
+from devops.models.network import Network
+from devops import settings
 
 
 class Shell(object):
     def __init__(self):
         self.params = self.get_params()
-        if getattr(self.params, 'name', None):
+        if (getattr(self.params, 'name', None) and
+                getattr(self.params, 'command', None) != 'create'):
             self.env = Environment.get(name=self.params.name)
 
     def execute(self):
@@ -57,10 +65,16 @@ class Shell(object):
         self.env.erase()
 
     def do_start(self):
-        self.env.start()
+        if self.params.node_name is not None:
+            self.env.get_node(name=self.params.node_name).start()
+        else:
+            self.env.start()
 
     def do_destroy(self):
-        self.env.destroy(verbose=False)
+        if self.params.node_name is not None:
+            self.env.get_node(name=self.params.node_name).destroy()
+        else:
+            self.env.destroy(verbose=False)
 
     def do_suspend(self):
         self.env.suspend(verbose=False)
@@ -127,6 +141,100 @@ class Shell(object):
     def do_version(self):
         print(devops.__version__)
 
+    def do_create(self):
+        env_name = self.params.name
+        for env in Environment.list():
+            if env.name == env_name:
+                print("Please select another environment name")
+                raise SystemExit()
+        self.env = Environment.create(env_name)
+        networks, prefix = self.params.net_pool.split(':')
+        Network.default_pool = Network.create_network_pool(
+            networks=[ipaddr.IPNetwork(networks)],
+            prefix=int(prefix))
+        networks = Network.networks_create(environment=self.env)
+        admin_node = self.admin_add(networks=networks)
+        self.do_slave_add(force_define=False)
+        self.env.define()
+        admin_node.disk_devices.get(device='cdrom').volume.upload(
+            self.params.iso_path)
+        for net in self.env.get_networks():
+            net.start()
+
+    def do_slave_add(self, force_define=True):
+        vcpu = self.params.vcpu_count
+        memory = self.params.ram_size
+        created_nodes = len(self.env.get_nodes())
+        node_count = self.params.node_count
+
+        for node in xrange(created_nodes, created_nodes + node_count):
+            node_name = "slave-%i" % (node)
+            node = self.env.add_node(name=node_name, vcpu=vcpu, memory=memory)
+            disknames_capacity = {
+                'system': 50 * 1024 ** 3
+            }
+            if self.params.second_disk_size > 0:
+                disknames_capacity[
+                    'cinder'] = self.params.second_disk_size * 1024 ** 3
+            if self.params.third_disk_size > 0:
+                disknames_capacity[
+                    'swift'] = self.params.third_disk_size * 1024 ** 3
+            node.attach_disks(
+                disknames_capacity=disknames_capacity,
+                force_define=force_define)
+            node.attach_to_networks()
+            if force_define is True:
+                node.define()
+
+    def do_slave_remove(self):
+        volumes = []
+        for drive in self.env.get_node(
+                name=self.params.node_name).disk_devices:
+            volumes.append(drive.volume)
+        self.env.get_node(name=self.params.node_name).remove()
+        for volume in volumes:
+            volume.erase()
+
+    def do_slave_change(self):
+        node = self.env.get_node(name=self.params.node_name)
+        node.set_vcpu(vcpu=self.params.vcpu_count)
+        node.set_memory(memory=self.params.ram_size)
+
+    def do_admin_change(self):
+        node = self.env.get_node(name="admin")
+        node.set_vcpu(vcpu=self.params.vcpu_count)
+        node.set_memory(memory=self.params.ram_size)
+
+    def do_admin_setup(self):
+        admin_node = self.env.get_node(name='admin')
+        admin_node.destroy()
+        node_manager.admin_prepare_disks(admin_node=admin_node,
+                                         disk_size=self.params.admin_disk_size)
+        admin_node.start()
+        node_manager.admin_change_config(admin_node)
+        admin_node.await("admin", timeout=10 * 60)
+        node_manager.admin_wait_bootstrap(3000, self.env)
+
+    def admin_add(self, networks=None):
+        vcpu = self.params.admin_vcpu_count
+        ram = self.params.admin_ram_size
+        iso_path = self.params.iso_path
+        iso_size = _get_file_size(iso_path)
+
+        if not (iso_size > 0):
+            print("Please select iso file correct")
+            sys.exit(1)
+        if networks is None:
+            networks = []
+            interfaces = settings.INTERFACE_ORDER
+            for name in interfaces:
+                networks.append(self.env.create_networks(name))
+        return self.env.describe_admin_node(name="admin",
+                                            vcpu=vcpu,
+                                            networks=networks,
+                                            memory=ram,
+                                            iso_path=iso_path)
+
     commands = {
         'list': do_list,
         'show': do_show,
@@ -143,7 +251,13 @@ class Shell(object):
         'net-list': do_net_list,
         'time-sync': do_timesync,
         'revert-resume': do_revert_resume,
-        'version': do_version
+        'version': do_version,
+        'create': do_create,
+        'slave-add': do_slave_add,
+        'slave-change': do_slave_change,
+        'slave-remove': do_slave_remove,
+        'admin-setup': do_admin_setup,
+        'admin-change': do_admin_change
     }
 
     def get_params(self):
@@ -170,6 +284,54 @@ class Shell(object):
                                      action='store_const', const=True,
                                      help='show admin node ip addresses',
                                      default=False)
+        iso_path_parser = argparse.ArgumentParser(add_help=False)
+        iso_path_parser.add_argument('--iso-path', dest='iso_path',
+                                     help='select Fuel ISO path',
+                                     required=True)
+        admin_ram_parser = argparse.ArgumentParser(add_help=False)
+        admin_ram_parser.add_argument('--admin-ram', dest='admin_ram_size',
+                                      help='select admin node RAM size(MB)',
+                                      default=1536, type=int)
+        admin_vcpu_parser = argparse.ArgumentParser(add_help=False)
+        admin_vcpu_parser.add_argument('--admin-vcpu', dest='admin_vcpu_count',
+                                       help='select admin node VCPU count',
+                                       default=2, type=int)
+        admin_disk_size_parser = argparse.ArgumentParser(add_help=False)
+        admin_disk_size_parser.add_argument('--admin-disk-size',
+                                            dest='admin_disk_size',
+                                            help='set admin node disk '
+                                                 'size(GB)',
+                                            default=50, type=int)
+        ram_parser = argparse.ArgumentParser(add_help=False)
+        ram_parser.add_argument('--ram', dest='ram_size',
+                                help='Set node RAM size',
+                                default=1024, type=int)
+        vcpu_parser = argparse.ArgumentParser(add_help=False)
+        vcpu_parser.add_argument('--vcpu', dest='vcpu_count',
+                                 help='Set node VCPU count',
+                                 default=1, type=int)
+        node_count = argparse.ArgumentParser(add_help=False)
+        node_count.add_argument('--node-count', dest='node_count',
+                                help='How many nodes will be created',
+                                default=1, type=int)
+        net_pool = argparse.ArgumentParser(add_help=False)
+        net_pool.add_argument('--net-pool', dest='net_pool',
+                              help='Set ip network pool (cidr)',
+                              default="10.21.0.0/16:24", type=str)
+        second_disk_size = argparse.ArgumentParser(add_help=False)
+        second_disk_size.add_argument('--second-disk-size',
+                                      dest='second_disk_size',
+                                      help='Allocate second disk for node '
+                                           'with selected size(GB). '
+                                           'If 0 disk will not be allocated',
+                                      default=50, type=int)
+        third_disk_size = argparse.ArgumentParser(add_help=False)
+        third_disk_size.add_argument('--third-disk-size',
+                                     dest='third_disk_size',
+                                     help='Allocate third disk for node '
+                                          'with selected size(GB). '
+                                          'If 0 disk will not be allocated',
+                                     default=50, type=int)
         parser = argparse.ArgumentParser(
             description="Manage virtual environments. "
                         "For addional help use command with -h/--help")
@@ -186,10 +348,11 @@ class Shell(object):
         subparsers.add_parser('erase', parents=[name_parser],
                               help="Delete environment",
                               description="Delete environment and VMs on it")
-        subparsers.add_parser('start', parents=[name_parser],
+        subparsers.add_parser('start', parents=[name_parser, node_name_parser],
                               help="Start VMs",
                               description="Start VMs in selected environment")
-        subparsers.add_parser('destroy', parents=[name_parser],
+        subparsers.add_parser('destroy', parents=[name_parser,
+                                                  node_name_parser],
                               help="Destroy(stop) VMs",
                               description="Stop VMs in selected environment")
         subparsers.add_parser('suspend', parents=[name_parser],
@@ -242,4 +405,44 @@ class Shell(object):
                                           " sync time on VMs")
         subparsers.add_parser('version',
                               help="Show devops version")
+        subparsers.add_parser('create',
+                              parents=[name_parser, vcpu_parser,
+                                       node_count, ram_parser,
+                                       net_pool, iso_path_parser,
+                                       admin_disk_size_parser,
+                                       admin_ram_parser,
+                                       admin_vcpu_parser,
+                                       second_disk_size,
+                                       third_disk_size],
+                              help="Create new environment",
+                              description="Create an environment with "
+                              "master node and slaves"),
+        subparsers.add_parser('slave-add',
+                              parents=[name_parser, node_count,
+                                       ram_parser, vcpu_parser,
+                                       second_disk_size, third_disk_size],
+                              help="Add node",
+                              description="Add new node to environment")
+        subparsers.add_parser('slave-change',
+                              parents=[name_parser, node_name_parser,
+                                       ram_parser, vcpu_parser],
+                              help="Change node vcpu and memory config",
+                              description="Change count of vcpus and memory")
+        subparsers.add_parser('slave-remove',
+                              parents=[name_parser, node_name_parser],
+                              help="Remove node from environment",
+                              description="Remove selected node from "
+                              "environment")
+        subparsers.add_parser('admin-setup',
+                              parents=[name_parser, admin_disk_size_parser],
+                              help="Setup admin node",
+                              description="Setup admin node from iso")
+        subparsers.add_parser('admin-change',
+                              parents=[name_parser, admin_ram_parser,
+                                       admin_vcpu_parser],
+                              help="Change admin node vcpu and memory config",
+                              description="Change count of vcpus and memory "
+                                          "for admin node")
+        if len(sys.argv) == 1:
+            sys.argv.append("-h")
         return parser.parse_args()
