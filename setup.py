69ef7792cad37028df99d5c5f1eb8c2ead574ae6
Dmitry Tyzhnenko <dtyzhnenko@mirantis.com>
2015-02-13 19:03:32 +0200

    Bump version to 2.9.3
    Change-Id: Ia29331813c8530da1610d5ad0605f1c90f6bc170

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name='devops',
-    version='2.9.2',
+    version='2.9.3',
     description='Library for creating and manipulating virtual environments',
     author='Mirantis, Inc.',
     author_email='product@mirantis.com',
