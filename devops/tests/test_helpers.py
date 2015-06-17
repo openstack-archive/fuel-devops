17416b10853ac7d6a4dbdb42113d222150609c79
Przemyslaw Kaminski <pkaminski@mirantis.com>
2015-06-17 14:37:30 +0200

    Refactoring of helpers.py
    - Remove HttpServer which seems to be unused
    - Remove random
    - Refactor unreadable multiline string
    - Fix usage of logger variables
    - Filesize uses os.stat
    - Added test_helpers.py
    
    Change-Id: Ie833107847ffbd023e5819c5d2fe72bfb594befd
    Implements: blueprint system-tests-and-devops-refactoring

---
+++ devops/tests/test_helpers.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import unittest
+
+from devops.helpers import helpers
+
+
+class TestSnaphotList(unittest.TestCase):
+
+    def test_get_keys(self):
+        keys = helpers.get_keys(
+            'IP',
+            'NETMASK',
+            'GW',
+            'HOSTNAME',
+            'NAT_INTERFACE',
+            'DNS1',
+            'SHOWMENU',
+            'BUILD_IMAGES'
+        )
+
+        self.assertIn('<Wait>\n<Esc>\n<Wait>\n', keys)
+        self.assertIn('ip=IP', keys)
+        self.assertIn('netmask=NETMASK', keys)
+        self.assertIn('gw=GW', keys)
+        self.assertIn('dns1=DNS1', keys)
+        self.assertIn('hostname=HOSTNAME', keys)
+        self.assertIn('dhcp_interface=NAT_INTERFACE', keys)
+        self.assertIn('showmenu=SHOWMENU', keys)
+        self.assertIn('build_images=BUILD_IMAGES', keys)
