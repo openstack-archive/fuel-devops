1653d229449cd6bbad8a6f0d37a41fefde40eae7
Mikhail Samoylov <msamoylov@mirantis.com>
2016-03-17 15:45:45 +0300

    Change node' ip obtaining when https is forced
    We need to add a workaround for an obtaining
    ip address for node if the master node uses
    https by default. Otherwise we couldn't fetch
    it via fuel client because it doesn't
    support https proto.
    
    Closes bug: 1558527
    
    Change-Id: I70ee6fe0fc3deea3dcaad8b60cdb409de6f1be61

--- devops/helpers/helpers.py
+++ devops/helpers/helpers.py
@@ -184,13 +184,10 @@ def get_ip_from_json(js, mac):
 
 
 def get_slave_ip(env, node_mac_address):
-    with get_admin_remote(env) as remote:
-        result = remote.execute(
-            "KEYSTONE_USER={user} KEYSTONE_PASS={passwd} "
-            "fuel nodes --json".format(
-                user=KEYSTONE_CREDS['username'],
-                passwd=KEYSTONE_CREDS['password']))['stdout']
-        js = json.loads(''.join(result))
+    admin_ip = get_admin_ip(env)
+    url = "https://{}:8443".format(admin_ip)
+    keystone_url = "http://{}:5000/v2.0/".format(admin_ip)
+    js = get_nodes(url, keystone_url)
     return get_ip_from_json(js, node_mac_address)
 
 
@@ -489,3 +486,19 @@ def _get_file_size(path):
     """
 
     return os.stat(path).st_size
+
+
+def get_nodes(url, keystone_url):
+    endpoint = '/api/nodes'
+    keystone = KeystoneClient(username=KEYSTONE_CREDS['username'],
+                              password=KEYSTONE_CREDS['password'],
+                              tenant_name=KEYSTONE_CREDS['tenant_name'],
+                              auth_url=keystone_url,
+                              insecure=True)
+    token = keystone.auth_token
+    req = urllib2.Request('{}{}'.format(url, endpoint))
+    req.add_header("X-Auth-Token", token)
+    resp = urllib2.urlopen(req)
+    nodes = json.loads(resp.read())
+    logger.debug("Nodes: {}".format(nodes))
+    return nodes
