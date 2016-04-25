2a7161cda94a06f4793edf4cf9b09c43fc74f336
Alexey Stepanov <astepanov@mirantis.com>
2016-04-25 18:24:38 +0300

    keystoneauth for getting nodes
     *  Replace manual implementation of keystone connection
        by standard keystoneauth.session
     *  Remove sphinx dependency: it's not used inside package
    
    PROS: easier to maintain, less code.
    
    Related-Bug: 1542275
    Related-Bug: 1558527
    Related-Bug: 1564913
    
    Change-Id: I0564fa103c79d6dc36cbfde526876cd32d6a0a01
    (cherry picked from commit a6040b8)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#    Copyright 2013 - 2015 Mirantis, Inc.
+#    Copyright 2013 - 2016 Mirantis, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -34,6 +34,7 @@ setup(
         (os.path.expanduser('~/.devops/log'), [])],
     scripts=['bin/dos.py'],
     install_requires=[
+        'keystoneauth1>=2.1.0',
         'xmlbuilder',
         'netaddr',
         'paramiko',
