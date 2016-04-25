fde07a573b454bc2e0cbe3714b0634caaba84734
Anton Studenov <astudenov@mirantis.com>
2016-04-25 12:42:36 +0300

    Fix slave IP address getting Change node' ip obtaining when https is forced Disable ssl verification for master node connection if it's supported
    This is a backporting of the following patches to master branch:
    https://review.openstack.org/#/c/277900/
    https://review.openstack.org/#/c/294002/
    https://review.openstack.org/#/c/301051/
    
    Related-Bug: 1542275
    Related-Bug: 1558527
    Related-Bug: 1564913
    
    Change-Id: I31c2f0f810dd36dc2d9aec7e1015b3fd322c6cc2

--- devops/settings.py
+++ devops/settings.py
@@ -47,7 +47,8 @@ DATABASES = {
 }
 
 KEYSTONE_CREDS = {'username': os.environ.get('KEYSTONE_USERNAME', 'admin'),
-                  'password': os.environ.get('KEYSTONE_PASSWORD', 'admin')}
+                  'password': os.environ.get('KEYSTONE_PASSWORD', 'admin'),
+                  'tenant_name': os.environ.get('KEYSTONE_TENANT', 'admin')}
 
 SSH_CREDENTIALS = {
     'admin_network': os.environ.get('ENV_ADMIN_NETWORK', 'admin'),
