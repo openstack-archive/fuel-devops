3f4e4254fa73bf496d61c639949ca5d43faee7a3
krozin <krozin@mirantis.com>
2016-05-20 19:07:48 +0300

    Support to start,stop,reset node via IPMI
    - implements basic functionality for baremetal nodes management:
      start, stop, reset, set boot device.
    
    - Node.has_snapshot() will return True for backward compatibility with
      fuel-qa code.
    
    - Node.get_snapshots() will return empty list [] for backward
      compatibility with fuel-qa code.
    
    - All other methods related to libvirt snapshot functionality, do
      nothing for baremetal driver.
    
    Driver usage:
    
    * In the devops YAML template, for baremetal node group
      set the driver name as the following:
    
         - name: baremetal-rack-01
           driver:
             name: devops.driver.baremetal.ipmi_driver
    
    * 'params' field for Node object contains the following attributes:
    
           nodes:
            - name: slave-01  # Custom name of baremetal for Fuel slave node
              role: fuel_slave  # Fixed role for Fuel master node properties
              params:
                boot: pxe   # one of: pxe, disk, cdrom, floppy
                ipmi_host: <ipmi address of the baremetal node>
                ipmi_user: <ipmi username>
                ipmi_password: <ipmi password>
                ipmi_previlegies: OPERATOR
                ipmi_lan_interface: lanplus
                ipmi_port: 623
                impi_cmd: ipmitool
                interfaces:  # interfaces for correct interface mapping
                 - label: iface1
                   mac_address: xx:xx:xx:xx:xx:xx
                 - label: iface2
                   mac_address: yy:yy:yy:yy:yy:yy
                network_config:  # openstack networks mapped to interfaces
                  iface1:
                    networks:
                     - fuelweb_admin  ## OpenStack network, NOT switch name
                     - public  ## OpenStack network, NOT switch name
                  iface2:
                    networks:
                     - storage  ## OpenStack network, NOT switch name
                     - management  ## OpenStack network, NOT switch name
                     - private  ## OpenStack network, NOT switch name
    
    Partial replacement for https://review.openstack.org/#/c/208000/
    
    Change-Id: I66ce3965717d7573fcbcdaf70fdf693bbda183f8
    blueprint: devops-bare-metal-driver

---
+++ devops/tests/driver/baremetal/test_ipmi_template.py
@@ -0,0 +1,174 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+import mock
+import yaml
+
+from devops.models import Environment
+from devops.driver.baremetal.ipmi_client import IpmiClient
+from django.test import TestCase
+
+ENV_TMPLT="""
+---
+aliases:
+
+  dynamic_address_pool:
+   - &pool_default 10.109.0.0/16:24
+
+  default_interface_model:
+   - &interface_model e1000
+
+template:
+  devops_settings:
+    env_name: test_ipmi
+
+    address_pools:
+    # Network pools used by the environment
+      fuelweb_admin-pool01:
+        net: *pool_default
+        params:
+          tag: 0
+
+    groups:
+     - name: baremetal-rack-01
+       driver:
+         name: devops.driver.baremetal
+
+       network_pools:  # Address pools for OpenStack networks.
+         # Actual names should be used for keys
+         # (the same as in Nailgun, for example)
+         fuelweb_admin: fuelweb_admin-pool01
+
+       l2_network_devices:  # bridges. It is *NOT* Nailgun networks
+         switch01:
+           address_pool: fuelweb_admin-pool01
+
+       nodes:
+        - name: slave-01  # Custom name of baremetal for Fuel slave node
+          role: fuel_slave  # Fixed role for Fuel master node properties
+          params:
+            ipmi_user: user1
+            ipmi_password: pass1
+            ipmi_previlegies: OPERATOR
+            ipmi_host: ipmi-1.host.address.net
+            ipmi_lan_interface: lanplus
+            ipmi_port: 623
+
+            # so, interfaces can be turn on in one or in a different switches.
+            interfaces: # First interface is not used
+             - label: iface2
+               l2_network_device: switch01
+            network_config:
+             iface2:
+                 networks:
+                    - fuelweb_admin  ## OpenStack network, NOT switch name
+
+        - name: slave-02  # Custom name of baremetal for Fuel slave node
+          role: fuel_slave  # Fixed role for Fuel master node properties
+          params:
+            ipmi_user: user2
+            ipmi_password: pass2
+            ipmi_previlegies: OPERATOR
+            ipmi_host: ipmi-2.host.address.net
+            ipmi_lan_interface: lanplus
+            ipmi_port: 623
+
+            # so, interfaces can be turn on in one or in a different switches
+            interfaces: # First interface is not used
+             - label: iface2
+               l2_network_device: switch01
+            network_config:
+              iface2:
+                 networks:
+                    - fuelweb_admin  ## OpenStack network, NOT switch name
+"""
+
+
+class TestIPMITemplate(TestCase):
+
+    def patch(self, *args, **kwargs):
+        patcher = mock.patch(*args, **kwargs)
+        mtmp = patcher.start()
+        self.addCleanup(patcher.stop)
+        return mtmp
+
+
+    def setUp(self):
+        super(TestIPMITemplate, self).setUp()
+
+        # Create Environment
+        self.full_conf = yaml.load(ENV_TMPLT)
+        self.env = Environment.create_environment(self.full_conf)
+        self.ipmiclient_mock = self.patch(
+            'devops.driver.baremetal.ipmi_driver.IpmiClient')
+        self.wait_mock = self.patch(
+            'devops.driver.baremetal.ipmi_driver.wait')
+        self.ipmiclient1 = mock.Mock(spec=IpmiClient)
+        self.ipmiclient2 = mock.Mock(spec=IpmiClient)
+        def get_client(
+            user, password, remote_host,
+            level = 'OPERATOR',
+            remote_lan_interface = 'lanplus',
+            remote_port = None, nodename = None):
+            if nodename == 'slave-01':
+                return self.ipmiclient1
+            elif nodename == 'slave-02':
+                return self.ipmiclient2
+
+        self.ipmiclient_mock.side_effect = get_client
+
+
+    def test_db(self):
+        node = self.env.get_node(name='slave-01')
+        assert(node.ipmi_user) == 'user1'
+        assert (node.ipmi_password) == 'pass1'
+        assert (node.ipmi_previlegies) == 'OPERATOR'
+        assert (node.ipmi_host) == 'ipmi-1.host.address.net'
+        assert (node.ipmi_lan_interface) == 'lanplus'
+        assert (node.ipmi_port) == 623
+
+        node2 = self.env.get_node(name='slave-02')
+        assert (node2.ipmi_user) == 'user2'
+        assert (node2.ipmi_password) == 'pass2'
+        assert (node2.ipmi_previlegies) == 'OPERATOR'
+        assert (node2.ipmi_host) == 'ipmi-2.host.address.net'
+        assert (node2.ipmi_lan_interface) == 'lanplus'
+        assert (node2.ipmi_port) == 623
+
+
+    def test_life_cycle(self):
+        self.env.define()
+
+        self.env.start()
+        assert self.ipmiclient_mock.call_count == 2
+        self.ipmiclient_mock.assert_any_call('user1', 'pass1',
+                                                'ipmi-1.host.address.net',
+                                                'OPERATOR',
+                                                'lanplus',
+                                                623,
+                                                'slave-01')
+        self.ipmiclient_mock.assert_any_call('user2', 'pass2',
+                                             'ipmi-2.host.address.net',
+                                             'OPERATOR',
+                                             'lanplus',
+                                             623,
+                                             'slave-02')
+        self.ipmiclient1.power_on.assert_called_once_with()
+
+        self.env.destroy()
+        self.ipmiclient1.power_off.assert_called_once_with()
+
+        self.env.erase()
+        self.ipmiclient1.power_off.assert_called_once_with()
