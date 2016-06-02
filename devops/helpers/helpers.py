9454cd54f8f3add70c3b0bab8a8be24d57d7b264
Artem Grechanichenko <agrechanichenko@mirantis.com>
2016-06-02 12:59:59 +0300

    Add centos_master node extension
    1) Add centos node extension
    2) add helper to build iso with settion
    
    Change-Id: I30f1914fd4516bda9538a3de590c027d07cd334a

--- devops/helpers/helpers.py
+++ devops/helpers/helpers.py
@@ -20,6 +20,7 @@ from functools import reduce
 # pylint: enable=redefined-builtin
 import os
 import socket
+import subprocess
 import time
 from warnings import warn
 
@@ -322,3 +323,86 @@ def get_nodes(admin_ip):
         endpoint_filter={'service_type': 'fuel'}
     )
     return nodes.json()
+
+
+def generate_cloud_image_settings(cloud_image_settings_path, admin_network,
+                                  interface_name, admin_ip, admin_netmask,
+                                  gateway, dns, dns_ext,
+                                  hostname, user, password):
+
+    # create dir for meta_data, user_data and cloud_ISO
+    dir_path = os.path.dirname(cloud_image_settings_path)
+
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    meta_data_path = os.path.join(dir_path,
+                                  "meta-data")
+    user_data_path = os.path.join(dir_path,
+                                  "user-data")
+
+    # create meta_data and user_data
+
+    meta_data_context = {
+        "interface_name": interface_name,
+        "address": admin_ip,
+        "network": admin_network,
+        "netmask": admin_netmask,
+        "gateway": gateway,
+        "dns": dns,
+        "dns_ext": dns_ext,
+        "hostname": hostname
+    }
+
+    meta_data_content = ("instance-id: iid-local1\n"
+                         "network-interfaces: |\n"
+                         " auto {interface_name}\n"
+                         " iface {interface_name} inet static\n"
+                         " address {address}\n"
+                         " network {network}\n"
+                         " netmask {netmask}\n"
+                         " gateway {gateway}\n"
+                         " dns-nameservers {dns} {dns_ext}\n"
+                         "local-hostname: {hostname}")
+
+    logger.debug("meta_data contains next data: \n{}".format(
+        meta_data_content.format(**meta_data_context)))
+
+    with open(meta_data_path, 'w') as f:
+        f.write(meta_data_content.format(**meta_data_context))
+
+    user_data_context = {
+        "interface_name": interface_name,
+        "gateway": gateway,
+        "user": user,
+        "password": password
+    }
+
+    user_data_content = ("\n#cloud-config\n"
+                         "ssh_pwauth: True\n"
+                         "chpasswd:\n"
+                         " list: |\n"
+                         "  {user}:{password}\n"
+                         " expire: False \n\n"
+                         "runcmd:\n"
+                         " - sudo ifup {interface_name}\n"
+                         " - sudo sed -i -e '/^PermitRootLogin/s/^"
+                         ".*$/PermitRootLogin yes/' /etc/ssh/sshd_config\n"
+                         " - sudo service ssh restart\n"
+                         " - sudo route add default gw "
+                         "{gateway} {interface_name}")
+
+    logger.debug("user_data contains next data: \n{}".format(
+        user_data_content.format(**user_data_context)))
+
+    with open(user_data_path, 'w') as f:
+        f.write(user_data_content.format(**user_data_context))
+
+    # Generate cloud_ISO
+    cmd = "genisoimage -output {} " \
+          "-volid cidata -joliet " \
+          "-rock {} {}".format(cloud_image_settings_path,
+                               user_data_path,
+                               meta_data_path)
+
+    subprocess.check_call(cmd, shell=True)
