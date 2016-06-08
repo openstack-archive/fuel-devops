5a408e0e404bd383fea62be983239c9225da1e79
Anton Studenov <astudenov@mirantis.com>
2016-06-08 12:39:42 +0300

    Add centos_master node extension
    1) Add centos node extension
    2) add helper to build iso with settings for booting centos master node
    3) add unit tests
    
    Change-Id: I30f1914fd4516bda9538a3de590c027d07cd334a
    Implements blueprint: master-node-installation-as-devops-extension

---
+++ devops/driver/empty/__init__.py
@@ -0,0 +1,29 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from devops.driver.empty.driver import EmptyDiskDevice as DiskDevice
+from devops.driver.empty.driver import EmptyDriver as Driver
+from devops.driver.empty.driver import EmptyInterface as Interface
+from devops.driver.empty.driver import EmptyL2NetworkDevice as L2NetworkDevice
+from devops.driver.empty.driver import EmptyVolume as Volume
+from devops.driver.empty.driver import EmptyNode as Node
+
+__all__ = [
+    'DiskDevice',
+    'Driver',
+    'Interface',
+    'L2NetworkDevice',
+    'Volume',
+    'Node',
+]
