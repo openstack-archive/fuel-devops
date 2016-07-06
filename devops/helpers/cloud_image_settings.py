7df39ec6847b57731f5c397dfa24928a42ce5069
Dennis Dmitriev <ddmitriev@mirantis.com>
2016-07-06 21:42:57 +0300

    Configure cloudimage-based nodes from devops template
    - New attributes in Libvirt Node object:
      cloud_init_volume_name: <string>  # Volume name for cloudinit config
          If set, than Node.define() will try to make an ISO image with
          metadata for cloudimage, and upload it to the specified Volume.
      cloud_init_iface_up: <string>  # Interface label to set up on boot
    
    - New attributes in Libvirt Volume object for cloudinit metadata:
      cloudinit_meta_data: <string>  # Yaml as a string
      cloudinit_user_data: <string>  # Yaml as a string
    
    - Resize 'source_image' after upload to the specified 'capacity' size.
      It's useful for cases when original qcow2 is much smaller than
      it is required, so 'source_image' with the size 2Gb will be resized
      to 'capacity', for example 50Gb, after upload to the libvirt pool.
      For qcow2 images, real disk space is not allocated, it is a virtual
      resize only.
    
    - 'centos_master' node role extension refactored: now pre_define()
      method just initialize the metadata objects (if they weren't passed
      from the template).
    
    - add an example template
    
    Closes-Bug:#1602298
    
    Change-Id: If35b252964a28a4bd43f073318cee93f3dfbe907

--- devops/helpers/cloud_image_settings.py
+++ devops/helpers/cloud_image_settings.py
@@ -18,21 +18,19 @@ import subprocess
 from devops import logger
 
 
-def generate_cloud_image_settings(cloud_image_settings_path, admin_network,
+def generate_cloud_image_settings(cloud_image_settings_path, meta_data_path,
+                                  user_data_path,  admin_network,
                                   interface_name, admin_ip, admin_netmask,
-                                  gateway, dns, dns_ext,
-                                  hostname, user, password):
+                                  gateway,
+                                  hostname,
+                                  meta_data_content=None,
+                                  user_data_content=None):
 
     # create dir for meta_data, user_data and cloud_ISO
     dir_path = os.path.dirname(cloud_image_settings_path)
 
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
-
-    meta_data_path = os.path.join(dir_path,
-                                  "meta-data")
-    user_data_path = os.path.join(dir_path,
-                                  "user-data")
 
     # create meta_data and user_data
 
@@ -42,21 +40,20 @@ def generate_cloud_image_settings(cloud_image_settings_path, admin_network,
         "network": admin_network,
         "netmask": admin_netmask,
         "gateway": gateway,
-        "dns": dns,
-        "dns_ext": dns_ext,
         "hostname": hostname
     }
 
-    meta_data_content = ("instance-id: iid-local1\n"
-                         "network-interfaces: |\n"
-                         " auto {interface_name}\n"
-                         " iface {interface_name} inet static\n"
-                         " address {address}\n"
-                         " network {network}\n"
-                         " netmask {netmask}\n"
-                         " gateway {gateway}\n"
-                         " dns-nameservers {dns} {dns_ext}\n"
-                         "local-hostname: {hostname}")
+    if meta_data_content is None:
+        meta_data_content = ("instance-id: iid-local1\n"
+                             "network-interfaces: |\n"
+                             " auto {interface_name}\n"
+                             " iface {interface_name} inet static\n"
+                             " address {address}\n"
+                             " network {network}\n"
+                             " netmask {netmask}\n"
+                             " gateway {gateway}\n"
+                             " dns-nameservers 8.8.8.8\n"
+                             "local-hostname: {hostname}")
 
     logger.debug("meta_data contains next data: \n{}".format(
         meta_data_content.format(**meta_data_context)))
@@ -67,23 +64,22 @@ def generate_cloud_image_settings(cloud_image_settings_path, admin_network,
     user_data_context = {
         "interface_name": interface_name,
         "gateway": gateway,
-        "user": user,
-        "password": password
     }
 
-    user_data_content = ("\n#cloud-config\n"
-                         "ssh_pwauth: True\n"
-                         "chpasswd:\n"
-                         " list: |\n"
-                         "  {user}:{password}\n"
-                         " expire: False \n\n"
-                         "runcmd:\n"
-                         " - sudo ifup {interface_name}\n"
-                         " - sudo sed -i -e '/^PermitRootLogin/s/^"
-                         ".*$/PermitRootLogin yes/' /etc/ssh/sshd_config\n"
-                         " - sudo service ssh restart\n"
-                         " - sudo route add default gw "
-                         "{gateway} {interface_name}")
+    if user_data_content is None:
+        user_data_content = ("\n#cloud-config\n"
+                             "ssh_pwauth: True\n"
+                             "chpasswd:\n"
+                             " list: |\n"
+                             "  root:r00tme\n"
+                             " expire: False \n\n"
+                             "runcmd:\n"
+                             " - sudo ifup {interface_name}\n"
+                             " - sudo sed -i -e '/^PermitRootLogin/s/^"
+                             ".*$/PermitRootLogin yes/' /etc/ssh/sshd_config\n"
+                             " - sudo service ssh restart\n"
+                             " - sudo route add default gw "
+                             "{gateway} {interface_name}")
 
     logger.debug("user_data contains next data: \n{}".format(
         user_data_content.format(**user_data_context)))
