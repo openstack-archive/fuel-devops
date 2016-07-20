341cbbda43daaaf02cab356514fb5f0134f0e8f6
Alexey Stepanov <astepanov@mirantis.com>
2016-07-20 13:57:49 +0300

    Paramiko 2.0.1 is buggy and could produce fails
    Ban paramiko 2.0.1
    Closes-bug: #1595865
    
    Change-Id: I27b26fa32fdfb3280a0af91ab5f3c031f69368ea

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     install_requires=[
         'keystoneauth1>=2.1.0',
         'netaddr>=0.7.12,!=0.7.16',
-        'paramiko>=1.16.0',
+        'paramiko>=1.16.0, !=2.0.1',
         'Django>=1.8,<1.9',
         'jsonfield',
         'PyYAML>=3.1.0',
