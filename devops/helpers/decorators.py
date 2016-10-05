e5402d3fddb75d17630fddae6452551a02f923b1
Alexey Stepanov <astepanov@mirantis.com>
2016-10-05 12:10:11 +0300

    Implement logwrap
    1. Copy from fuel-qa
    2. Implement pretty repr (partial copy of pretty_log)
    3. Implement logwrap (partial copy from fuel-qa, but with pretty_repr)
    
    Change-Id: I73c1c8909f6c67f8370600f6381a1773089cd516

--- devops/helpers/decorators.py
+++ devops/helpers/decorators.py
@@ -14,10 +14,16 @@
 
 from __future__ import unicode_literals
 
+import collections
 import functools
 import inspect
+import logging
+import sys
 import threading
 import time
+
+import mock  # Used by unit tests
+import six
 
 from devops import error
 from devops import logger
@@ -117,3 +123,176 @@ def retry(exception, count=10, delay=1):
         return wrapper
 
     return decorator
+
+
+# pylint: disable=no-member
+def _get_arg_names(func):
+    """get argument names for function
+
+    :param func: func
+    :return: list of function argnames
+    :rtype: list
+
+    >>> def tst_1():
+    ...     pass
+
+    >>> _get_arg_names(tst_1)
+    []
+
+    >>> def tst_2(arg):
+    ...     pass
+
+    >>> _get_arg_names(tst_2)
+    ['arg']
+    """
+    # noinspection PyUnresolvedReferences
+    return (
+        [arg for arg in inspect.getargspec(func=func).args] if six.PY2 else
+        list(inspect.signature(obj=func).parameters.keys())
+    )
+
+
+def _getcallargs(func, *positional, **named):
+    """get real function call arguments without calling function
+
+    :rtype: dict
+    """
+    # noinspection PyUnresolvedReferences
+    if sys.version_info[0:2] < (3, 5):  # apply_defaults is py35 feature
+        orig_args = inspect.getcallargs(func, *positional, **named)
+        # Construct OrderedDict as Py3
+        arguments = collections.OrderedDict(
+            [(key, orig_args[key]) for key in _get_arg_names(func)]
+        )
+        if six.PY2:
+            # args and kwargs is not bound in py27
+            # Note: py27 inspect is not unicode
+            if 'args' in orig_args:
+                arguments[b'args'] = orig_args['args']
+            if 'kwargs' in orig_args:
+                arguments[b'kwargs'] = orig_args['kwargs']
+        return arguments
+    sig = inspect.signature(func).bind(*positional, **named)
+    sig.apply_defaults()  # after bind we doesn't have defaults
+    return sig.arguments
+# pylint:enable=no-member
+
+
+def pretty_repr(src, indent=0, no_indent_start=False):
+    """Make human readable repr of object
+
+    :param src: object to process
+    :type src: object
+    :param indent: start indentation, all next levels is +4
+    :type indent: int
+    :param no_indent_start: do not indent open bracket and simple parameters
+    :type no_indent_start: bool
+    :return: formatted string
+    """
+    formatters = {
+        'simple': "{spc:<{indent}}{val!r}",
+        'dict': "\n{spc:<{indent}}{key!r:{size}}: {val},",
+        'iterable': "\n{spc:<{indent}}{elem!r},"
+    }
+    if not isinstance(src, (list, set, tuple, dict)):
+        return formatters['simple'].format(
+            spc='',
+            indent=0 if no_indent_start else indent,
+            val=src
+        )
+    if isinstance(src, dict):
+        prefix, suffix = '{', '}'
+        result = ''
+        max_len = len(max([repr(key) for key in src]))
+        for key, val in src.items():
+            result += formatters['dict'].format(
+                spc='',
+                indent=indent + 4,
+                size=max_len,
+                key=key,
+                val=pretty_repr(val, indent + 8, no_indent_start=True)
+            )
+        return (
+            '\n{start:>{indent}}'.format(
+                start=prefix,
+                indent=indent
+            ) +
+            result +
+            '\n{end:>{indent}}'.format(end=suffix, indent=indent)
+        )
+    if isinstance(src, list):
+        prefix, suffix = '[', ']'
+    elif isinstance(src, tuple):
+        prefix, suffix = '(', ')'
+    else:
+        prefix, suffix = '{', '}'
+    result = ''
+    for elem in src:
+        result += formatters['iterable'].format(
+            spc='',
+            indent=indent + 4,
+            elem=elem
+        )
+    return (
+        '\n{start:>{indent}}'.format(
+            start=prefix,
+            indent=indent) +
+        result +
+        '\n{end:>{indent}}'.format(end=suffix, indent=indent)
+    )
+
+
+def logwrap(log=logger, log_level=logging.DEBUG, exc_level=logging.ERROR):
+    """Log function calls
+
+    :type log: logging.Logger
+    :type log_level: int
+    :type exc_level: int
+    :rtype: callable
+    """
+    def real_decorator(func):
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            call_args = _getcallargs(func, *args, **kwargs)
+            args_repr = ""
+            if len(call_args) > 0:
+                args_repr = "\n    " + "\n    ".join((
+                    "{key!r}={val}".format(
+                        key=key,
+                        val=pretty_repr(val, indent=8, no_indent_start=True)
+                    )
+                    for key, val in call_args.items())
+                ) + '\n'
+            log.log(
+                level=log_level,
+                msg="Calling: \n{name!r}({arguments})".format(
+                    name=func.__name__,
+                    arguments=args_repr
+                )
+            )
+            try:
+                result = func(*args, **kwargs)
+                log.log(
+                    level=log_level,
+                    msg="Done: {name!r} with result:\n{result}".format(
+                        name=func.__name__,
+                        result=pretty_repr(result))
+                )
+            except BaseException:
+                log.log(
+                    level=exc_level,
+                    msg="Failed: \n{name!r}({arguments})".format(
+                        name=func.__name__,
+                        arguments=args_repr,
+                    ),
+                    exc_info=True
+                )
+                raise
+            return result
+        return wrapped
+
+    if not isinstance(log, (logging.Logger, mock.Mock)):
+        func, log = log, logger
+        return real_decorator(func)
+
+    return real_decorator
