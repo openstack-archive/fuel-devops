84b078849533a9c46555e2d79c0b5263ad957f95
Hervé Beraud <hberaud@redhat.com>
2018-10-15 14:49:06 +0200

    Using openstack pbr to build package
    Remove package version management, manage version with git tags
    Using openstack packaging best practices
    
    Change-Id: Ic135ee1349c2d2a41e8381956b997716dcc3615d

--- setup.py
+++ setup.py
@@ -12,23 +12,12 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import sys
-
 import setuptools
 
 
 setuptools.setup(
-    name='fuel-devops',
-    version='3.0.5',
-    description='Library for creating and manipulating virtual environments',
-    author='Mirantis, Inc.',
-    author_email='product@mirantis.com',
-    url='http://mirantis.com',
-    keywords='devops virtual environment',
-    zip_safe=False,
-    include_package_data=True,
-    packages=setuptools.find_packages(),
-    package_data={'devops': ['templates/*.yaml', 'templates/*.yml']},
+    setup_requires=['pbr>=2.0.0'],
+    pbr=True,
     scripts=[
         'bin/dos.py',
         'bin/dos-manage.py',
@@ -36,32 +25,4 @@ setuptools.setup(
         'bin/dos_check_system.sh',
         'bin/dos_check_packages.sh',
         'bin/dos_check_db.sh',
-    ],
-    data_files=[('bin', ['bin/dos_functions.sh'])],
-    # Use magic in install_requires due to risk of old setuptools
-    install_requires=[
-        'keystoneauth1>=2.1.0',
-        'netaddr>=0.7.12,!=0.7.16',
-        'paramiko>=1.16.0,!=2.0.1',
-        'Django>=1.8,<1.9',
-        'jsonfield',
-        'PyYAML>=3.1.0',
-        'libvirt-python>=3.5.0,<4.1.0',
-        'tabulate',
-        'six>=1.9.0',
-        'python-dateutil>=2.4.2',
-        'lxml',
-        'enum34' if sys.version_info.major == 2 else '',
-        'fasteners>=0.7.0',
-        'virtualbmc'
-    ],
-    tests_require=[
-        'pytest>=2.7.1',
-        'pytest-django >= 2.8.0',
-        'mock>=1.2',
-        'tox>=2.0'
-    ],
-    extras_require={
-        'postgre': ["psycopg2"],
-    }
-)
+    ],)
